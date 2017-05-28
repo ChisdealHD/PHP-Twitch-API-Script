f2b90047923e9e41ad81b3c492163d17a9151244
ChisdealHD <Chisdeal2014@hotmail.com>
2017-05-28 20:54:15 +0100

    Twitch API BASIC
    BAsic Coding Twitch API!

---
+++ gametitle.php
@@ -0,0 +1,57 @@
+<?php 
+    //grabs the parameters of the url ?user=
+    $url = "https://api.twitch.tv/kraken/streams/ChisdealHD?client_id=";
+    $content = file_get_contents($url);
+    $json = json_decode($content, true);
+    $game = $json['stream']['channel']['game'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>ChisdealHD LIVE PAGE!!</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/portfolio-item.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+<script>
+window.setInterval("reloadIFrame();", 600000);
+
+function reloadIFrame() {
+ document.frames["Refresh"].location.reload();
+}
+</script> 
+	
+</head>
+
+<body>
+    <meta http-equiv="refresh" content="30">
+	<h1><small id="Refresh"><?php echo "<br>Game: $game</br>";?></small>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
